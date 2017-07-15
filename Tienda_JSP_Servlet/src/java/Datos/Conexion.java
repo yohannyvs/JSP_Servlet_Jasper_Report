88b576e690223e45a10115ec5e3b486e6fbdc042
yohannyvs <yohannyvs@gmail.com>
2017-07-14 22:57:00 -0600

    act 2
    Conexion con Postgres - local

--- Tienda_JSP_Servlet/src/java/Datos/Conexion.java
+++ Tienda_JSP_Servlet/src/java/Datos/Conexion.java
@@ -1,6 +1,28 @@
 
 package Datos;
 
-public class Conexion {
+import java.sql.*;
+
+public class Conexion 
+{
+    Connection cn;
+    private String driver = "org.postgresql.Driver";
+    private String cadena = "jdbc:postgresql://localhost:5432/Tienda";;
+    private String usuario = "postgres";
+    private String password = "progra";
     
+    public Connection con()
+    {
+        try {
+           Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            cn = DriverManager.getConnection(cadena, usuario, password);
+            if (cn != null) {
+                System.out.println("Conexión correcta.");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Error : " + e.getMessage());
+        }
+        return cn;
+    }
 }
